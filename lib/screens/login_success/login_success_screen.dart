2f992c56fb563cc5481d42145912f8fcae78faa6
EdogaCE <edogadaniel.ce@gmail.com>
2023-07-18 04:25:08 -0400

    first commit

---
+++ lib/screens/login_success/login_success_screen.dart
@@ -0,0 +1,19 @@
+// ignore_for_file: use_key_in_widget_constructors, prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+import 'components/body.dart';
+
+class LoginSuccessScreen extends StatelessWidget {
+  static String routeName = "/login_success";
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: SizedBox(),
+        title: Text("Login Success"),
+      ),
+      body: Body(),
+    );
+  }
+}
